87c5c0c3793ca688b5c849ca406b30436a9b92fa
Hesham-Abdelbast <heshamabdelbast87@gmial.com>
2026-01-21 20:31:29 +0200

    change test only

--- src/app/core/api.config.ts
+++ src/app/core/api.config.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL = 'https://mepsol.azurewebsites.net/api';
 // export const API_BASE_URL = 'http://localhost:5202/api';
 // export const API_BASE_URL = 'https://localhost:7003/api';
+//
